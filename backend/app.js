81a6e914b2e3bb38a2dbddb1d7a2b707c1d4143c
PeymonA <peymon.armand4@gmail.com>
2025-09-06 13:00:38 +1200

    dockerized

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,12 @@
 var dotenv = require('dotenv');
-dotenv.config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+
+dotenv.config();
 
 var indexRouter = require('./routes/index');
 
